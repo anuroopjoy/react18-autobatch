8a37dc11b9086df5a530004a616f97316fc89794
Anuroop Joy <anuroop.joy@hrblock.com>
2021-06-14 23:02:22 +0530

    class

--- src/ClassApp.js
+++ src/ClassApp.js
@@ -32,17 +32,6 @@ class App extends React.Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
                     <button onClick={this.handleClick}>Increment</button>
                     <div>
                         {this.state.valid.toString()} - {this.state.count}
